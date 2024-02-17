8d4fcceac84db25b9fe478d4fc4d77bdbaadf0f5
EnriLion <enriqueleon0201leon@gmail.com>
2024-02-16 21:58:56 -0600

    Updating files

---
+++ linux-unix/bash/hackerank/letsecho.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Write  a bash script that prints the string "HELLO"
+# Input Format
+#
+# There is no input file required for this problem.
+# Output Format
+# HELLO
+# Sample Imput
+# -
+# Sample Output
+# HELLO
+# Explanation
+# -
+echo HELLO
+# printf HELLO
